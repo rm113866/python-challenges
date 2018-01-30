9beb2ef704e8476adcedf9f8b3b24c8456f3c44a
rm113866 <35816944+rm113866@users.noreply.github.com>
2018-01-30 13:45:48 -0700

    Add files via upload

---
+++ python challenge  1.py
@@ -0,0 +1,9 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("How are you?")
+How are you?
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> 
