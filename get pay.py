6a074ff1b03313e317a4cc1aab6c293ace62ef81
rm113866 <35816944+rm113866@users.noreply.github.com>
2018-02-16 12:04:16 -0700

    Delete get pay.py

--- get pay.py
+++
@@ -1,8 +0,0 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> hour = input('Enter Hour: '); rate = input('Enter Rate: '); pay = int(hour) * float(rate)
-Enter Hour: 35
-Enter Rate: 2.75
->>> print(pay)
-96.25
->>> 
