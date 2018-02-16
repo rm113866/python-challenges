3c5bfbf98c85fbd738ad2992e4960dbe6a05737a
rm113866 <35816944+rm113866@users.noreply.github.com>
2018-02-16 14:06:38 -0700

    Add files via upload

---
+++ python_text_challenge_3.py
@@ -0,0 +1,2 @@
+# Ryan Miller - text - 2/16/18 - challenge 3#
+>>> print("*\n**\n***\n****")
---
+++ python_text_challenge_4.py
@@ -0,0 +1,2 @@
+# Ryan Miller - text - 2/16/18 - challenge 4#
+>>> print("  *  \n *** \n*****\n *** \n*****\n *** \n*****\n  *  \n  *  ")
